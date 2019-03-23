cde9de9411978dd7483df184ff7958a314ec4866
Michal R <marelix2@gmail.com>
2019-03-23 21:49:30 +0100

    init of frontend

---
+++ front/src/components/usersDisplayer/UsersDisplayer.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import API from '../../endpoints'
+import axios from '../../AxiosApi'
+
+class UsersDisplayer extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      user: null
+    }
+  }
+
+  fetchUsers = () => {
+    axios.get(API.GET_USERS).then((response) => this.setState({ users: response.data.data }))
+  }
+
+  fetchUser = (id) => {
+    axios.get(API.GET_USER(id)).then((response) => this.setState({ user: response.data.data })) 
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+    this.fetchUser(1);
+  }
+
+
+  render() {
+    const { users, user } = this.state
+    return (
+      <>
+        {JSON.stringify(users, null, 4)}
+        <br/>
+        {JSON.stringify(user)}
+      </>
+    );
+  }
+}
+
+export default UsersDisplayer;
