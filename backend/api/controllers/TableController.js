90d3defc8fc4f7c1270c2682b48e82d5932f0d0e
Michal R <marelix2@gmail.com>
2019-03-23 20:49:52 +0100

    readme update

--- backend/api/controllers/TableController.js
+++ backend/api/controllers/TableController.js
@@ -1,43 +1,43 @@
-const {db} = require('./../../config/database.js')
+const { db } = require('./../../config/database.js')
 
 
 const TableController = () => {
 
-  getUsers =  (req, res, next) => {
+  const getUsers = (req, res, next) => {
     const sql = "select * from user"
     const params = []
 
     db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-    }
-
-   const getUser =  (req, res, next) => {
-      var sql = "select * from user where id = ?"
-      var params = [req.params.id]
-      db.get(sql, params, (err, row) => {
-          if (err) {
-            res.status(400).json({"error":err.message});
-            return;
-          }
-          res.json({
-              "message":"success",
-              "data":row
-          })
-        });
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
       }
+      res.json({
+        "message": "success",
+        "data": rows
+      })
+    });
+  }
+
+  const getUser = (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      res.json({
+        "message": "success",
+        "data": row
+      })
+    });
+  }
 
   return {
     getUsers,
     getUser
-}
+  }
 
 }
 
